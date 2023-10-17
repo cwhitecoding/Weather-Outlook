6d8cfd1d434de30eba84695b90f90b373d5c32d2
cwhitecoding <143142366+cwhitecoding@users.noreply.github.com>
2023-10-17 14:53:04 -0500

    add html and js

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+var apiKey = "7272f490779998b5e7d5c7ecc5fd3a2b";
+var forAPI = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key};
+var dailyAPI = api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key};
