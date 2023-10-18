4e94e23f2b812f2a0777fb6ebbf218336a445ba9
cwhitecoding <143142366+cwhitecoding@users.noreply.github.com>
2023-10-17 21:29:33 -0500

    Add classes and fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 var apiKey = "7272f490779998b5e7d5c7ecc5fd3a2b";
-var forAPI = "api.openweathermap.org/data/2.5/forecast?q=";
-var dailyAPI = "api.openweathermap.org/data/2.5/forecast?q=";
+var forAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var searchBtn = document.querySelector(".searchBtn");
-var cityInput = document.querySelector(".searchBox");
-var cityOutput = document.querySelector(".location");
-var weather = document.querySelector("#weather");
-var temp = document.querySelector("#temp");
-var wind = document.querySelector("#wind");
+var cityInput = $(".searchBox");
+var cityOutput = $(".location");
+var weather = $("#weather");
+var temp = $("#temp");
+var wind = $("#wind");
+var humidity = $("#humidity");
+
+// api.openweathermap.org/data/2.5/forecast?q=
+
+// {city name}
+
+// &appid={API key}
 
 
 searchBtn.addEventListener('click', function(){
-    fetch(forAPI+cityInput+"&appid="+apiKey)
+    console.log(cityInput.val());
+    fetch(forAPI + cityInput.val() + "&appid=" + apiKey + "&units=imperial")
     .then(function (response) {
         return response.json();
     })
