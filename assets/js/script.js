6b9910e1e3efc31342f6f96f918f0057c6bae299
cwhitecoding <143142366+cwhitecoding@users.noreply.github.com>
2023-10-20 13:11:33 -0500

    Update JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,32 @@ var temp = $("#temp");
 var wind = $("#wind");
 var humidity = $("#humidity");
 
-// api.openweathermap.org/data/2.5/forecast?q=
-
-// {city name}
-
-// &appid={API key}
-
-
-searchBtn.addEventListener('click', function(){
+searchBtn.addEventListener('click', function getInfo(){
     console.log(cityInput.val());
+    
     fetch(forAPI + cityInput.val() + "&appid=" + apiKey + "&units=imperial")
     .then(function (response) {
         return response.json();
     })
     .then(function(data){
         console.log(data);
+        var location = $("#location");
+        var cityName = data.name;
+        var icon2 = data.weather[0].icon;
+        var weatherDesc = data.weather.main;
+        var tempVal = data.main.temp;
+        var windSpd = data.wind.speed;
+        var icon = 'https://openweathermap.org/img/wn/' + icon2 + '@2x.png';
+        var iconPic = $('<img src="' + icon + '">');
+        
+
+        location.append(cityName);
+        icon.append(iconPic);
+        weather.text(weatherDesc);
+        temp.text(tempVal + "°F");
+        wind.text("Wind: " + (windSpd * 2.23694).toFixed(2) + " mph");
+        
+        console.log(cityName);
     })
 })
 
